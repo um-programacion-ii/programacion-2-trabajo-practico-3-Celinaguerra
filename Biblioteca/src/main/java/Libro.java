1159c100648595ab83d4764846e9f96df4b5655b
celi <celinaguerradiaz@gmail.com>
2025-04-26 18:22:25 -0300

    implemento clase libro

---
+++ Biblioteca/src/main/java/Libro.java
@@ -0,0 +1,39 @@
+public class Libro {
+
+    public enum Estado {
+        DISPONIBLE,
+        PRESTADO
+    }
+
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private Estado estado;
+
+    public Libro(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.estado = Estado.DISPONIBLE;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
